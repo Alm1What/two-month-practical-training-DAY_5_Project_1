3c186001d90c9d787266138327f5f8536f2c8a1a
evgen <zekanov16@gmail.com>
2025-04-07 19:52:10 +0300

    first commit

---
+++ src/main/java/org/example/day_5_practice_project_one/Day5PracticeProjectOneApplication.java
@@ -0,0 +1,13 @@
+package org.example.day_5_practice_project_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day5PracticeProjectOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day5PracticeProjectOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/day_5_practice_project_one/Day5PracticeProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day_5_practice_project_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day5PracticeProjectOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
